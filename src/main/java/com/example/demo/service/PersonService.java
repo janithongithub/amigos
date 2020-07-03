99158aa69ac61ba0a6cc71680873ef338233cb40
janitha_ch <janithconline@gmail.com>
2020-07-03 12:58:04 +0530

    person service

---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Person;
+
+public class PersonService {    // insert new person
+    
+
+    public int addPerson(Person person){
+        return 0;
+    }
+}
